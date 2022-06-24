e438b76862e3b8da1081e088f2024b4a2de2820c
Yang <38071744+crea9813@users.noreply.github.com>
2022-06-24 16:31:23 +0900

    fix: computeOffset

--- NMGLatLng+Extension.swift
+++ NMGLatLng+Extension.swift
@@ -7,32 +7,6 @@
 
 import NMapsMap
 
-public extension NMGLatLng {
-    internal var latLngRadians: LatLngRadians {
-        LatLngRadians(latitude: lat.radians, longitude: lng.radians)
-    }
-    
-    func heading(to: NMGLatLng) -> Double {
-        let bearing = Math.initialBearing(self.latLngRadians, to.latLngRadians)
-        return Math.wrap(value: bearing * (180 / .pi), min: 0, max: 360)
-    }
-    
-    func computeOffset(distance: Double,
-                       heading: Double) -> NMGLatLng {
-        let bearing = heading.radians
-        let latLng1 = self.latLngRadians
-        let sinLat2 = sin(latLng1.latitude) * cos(distance) + cos(latLng1.latitude) * sin(distance) * cos(bearing)
-        let lat2 = asin(sinLat2)
-        let y = sin(bearing) * sin(distance) * cos(latLng1.latitude)
-        let x = cos(distance) - sin(latLng1.latitude) * sinLat2
-        let lng2 = latLng1.longitude + atan2(y, x)
-        return NMGLatLng(
-            lat: lat2.degrees,
-            lng: Math.wrap(value: lng2.degrees, min: -180, max: 180)
-        )
-    }
-}
-
 extension Double {
     var radians: Double {
         return self * (.pi / 180)
--- NMapsCurve/Classes/NMapsCurvePath.swift
+++ NMapsCurve/Classes/NMapsCurvePath.swift
@@ -33,12 +33,12 @@ extension NMFCurvePath {
         let MO = R * cos(angle / 2)
         
         let heading = departure.heading(to: arrival)
-        let mCoordinate = departure.computeOffset(distance: ME / NMFCurvePath.EARTH_RADIUS, heading: heading)
+        let mCoordinate = computeOffset(from: departure, distance: ME / NMFCurvePath.EARTH_RADIUS, heading: heading)
         
         let direction = departure.lng - arrival.lng > 0 ? -1.0 : 1.0
         let angleFromCenter = 90.0 * direction
         
-        let oCoordinate = mCoordinate.computeOffset(distance: MO / NMFCurvePath.EARTH_RADIUS, heading: heading + angleFromCenter)
+        let oCoordinate = computeOffset(from: mCoordinate, distance: MO / NMFCurvePath.EARTH_RADIUS, heading: heading + angleFromCenter)
         
         var points: [NMGLatLng] = []
         
@@ -53,12 +53,40 @@ extension NMFCurvePath {
             let step = Double(i) * (degree / Double(num))
             let heading: Double = (-1.0) * direction
             
-            let pointOnCurvedLine = oCoordinate.computeOffset(distance: R / NMFCurvePath.EARTH_RADIUS, heading: initialHeading + heading * step)
+            let pointOnCurvedLine = computeOffset(from: oCoordinate, distance: R / NMFCurvePath.EARTH_RADIUS, heading: initialHeading + heading * step)
             points.append(NMGLatLng(lat: pointOnCurvedLine.lat, lng: pointOnCurvedLine.lng))
         }
         
         points = points.reversed()
         
         return points
+    }
+    
+    static private func computeOffset(
+        from: NMGLatLng,
+        distance: Double,
+        heading: Double) -> NMGLatLng {
+            let bearing = heading.radians
+            let latLng1 = from.latLngRadians
+            let sinLat2 = sin(latLng1.latitude) * cos(distance) + cos(latLng1.latitude) * sin(distance) * cos(bearing)
+            let lat2 = asin(sinLat2)
+            let y = sin(bearing) * sin(distance) * cos(latLng1.latitude)
+            let x = cos(distance) - sin(latLng1.latitude) * sinLat2
+            let lng2 = latLng1.longitude + atan2(y, x)
+            return NMGLatLng(
+                lat: lat2.degrees,
+                lng: Math.wrap(value: lng2.degrees, min: -180, max: 180)
+            )
+        }
+}
+
+public extension NMGLatLng {
+    internal var latLngRadians: LatLngRadians {
+        LatLngRadians(latitude: lat.radians, longitude: lng.radians)
+    }
+    
+    internal func heading(to: NMGLatLng) -> Double {
+        let bearing = Math.initialBearing(self.latLngRadians, to.latLngRadians)
+        return Math.wrap(value: bearing * (180 / .pi), min: 0, max: 360)
     }
 }
